7d007f873d455d5b8dbf7804b96858ba8aac2731
Akshat Mittal <akshatmittal2506@gmail.com>
2023-02-27 23:02:12 +0530

    built aesthetic screen saver

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const _time = document.querySelector(".time");
 const _date = document.querySelector(".date");
+const fullScreenBtn = document.querySelector(".full-screen-btn");
+const screenSaver = document.querySelector(".screen-saver");
+const screenSaverTime = document.querySelector(".screen-saver-time");
 
 const days = [
 	"Sunday",
@@ -40,6 +43,25 @@ ${Minutes < 10 ? `0${Minutes}` : Minutes}
 	_date.innerHTML = `
 ${days[Day]} , ${months[Month]} ${DatE}
 `;
+	screenSaverTime.innerHTML = `
+${Hours} :
+${Minutes < 10 ? `0${Minutes}` : Minutes} :
+${Seconds < 10 ? `0${Seconds}` : Seconds}
+`;
 };
 setTime();
 setInterval(setTime, 1000);
+
+fullScreenBtn.addEventListener("click", () => {
+	if (document.fullscreenElement) {
+		document.exitFullscreen();
+	} else {
+		document.documentElement.requestFullscreen();
+	}
+});
+
+document.addEventListener("fullscreenchange", () => {
+	console.log("fullscreenchange", screenSaver.classList);
+	screenSaver.classList.toggle("screen-saver-visible");
+	screenSaver.classList.toggle("screen-saver-invisible");
+});
