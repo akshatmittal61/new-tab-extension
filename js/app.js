fcb1e5e454122944e6a52f670f6e813e5a17933e
Akshat Mittal <akshatmittal2506@gmail.com>
2023-03-05 01:29:02 +0530

    fixed search bar

--- js/app.js
+++ js/app.js
@@ -4,5 +4,9 @@ searchForm.addEventListener("submit", (e) => {
 	e.preventDefault();
 	const searchInput = searchForm.querySelector("input");
 	const searchValue = searchInput.value;
-	window.location.href = `https://www.google.com/search?q=${searchValue}`;
+	const urlRegex =
+		/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/;
+	if (urlRegex.test(searchValue)) window.location.href = searchValue;
+	else
+		window.location.href = `https://www.google.com/search?q=${searchValue}`;
 });
